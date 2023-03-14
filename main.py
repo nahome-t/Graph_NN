cf438bbee938d4b81ed2024bb8003790046f9770
nahometewolde <nahome.t@icloud.com>
2023-03-14 14:35:29 +0000

    creates function that will train neural network given a model and its data

--- main.py
+++ main.py
@@ -28,3 +28,41 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = model.to(device)
 data = data.to(device)
 
+
+def train(model, data):
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    model.train()
+    test_accuracy(model=model, data=data, epoch_num=0)
+    for epoch in range(num_epochs):
+        optimizer.zero_grad()
+        output = model(data)
+
+        # data.train_mask/test_mask is a tensor of True/False values which
+        # filter the dataset to only include training or testing data
+        loss = F.nll_loss(output[data.train_mask], data.y[data.train_mask])
+        loss.backward() # Calculates the gradients
+        optimizer.step() # Updates the model via gradient descent
+        # test_accuracy(model, data, epoch+1) #+1 because epoch should start
+        # from 1
+    test_accuracy(model, data, num_epochs)
+
+def test_accuracy(model, data, epoch_num=None, on_training_data = False):
+    # Will test on the remaining data set
+    if epoch_num == 0:
+        print('CHECKING ACCURACY ON TRAINING DATA') if on_training_data else \
+            print('CHECKING ACCURACY ON TESTING DATA')
+    mask = data.train_mask if on_training_data else data.test_mask  # Used to
+    # filter dataset by training a
+
+    model.eval()
+    with torch.no_grad():
+        test_num = mask.sum()
+        prediction = model(data).argmax(dim=1)
+        correct = (prediction[mask] == data.y[mask]).sum()
+        acc = int(correct)/test_num
+        print(f'Epoch num: {epoch_num}, Accuracy {acc:.2f}, i.e. {correct}'
+              f'/{test_num}')
+    model.train()
+
+
+train(model=model, data=data)
