6e5dfb59eb9dc7b66ad96e6d80eef7f2caf9c2f7
nahometewolde <nahome.t@icloud.com>
2023-03-14 10:57:02 +0000

    Adds class that creates a regular graph convolutional neural network (of arbitrary  depth

---
+++ models.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+from torch import matmul, zeros
+from torch_geometric.nn import GCNConv, MessagePassing
+from torch_geometric.nn.conv.gcn_conv import gcn_norm
+from torch_geometric.utils import add_self_loops, degree
+import torch.functional as F
+
+
+class GCN(nn.Module):
+    def __init__(self, in_features, out_features, hidden_layer_size, depth=2):
+        super().__init__()
+        self.in_conv = GCNConv(in_features, hidden_layer_size)
+        self.convs = nn.ModuleList(
+            [GCNConv(hidden_layer_size, hidden_layer_size) for _
+             in range(depth - 2)])
+        self.out_conv = GCNConv(hidden_layer_size, out_features)
+
+    def forward(self, data):
+        h, edge_index = data.x, data.edge_index
+        h = self.in_conv(h, edge_index)
+        h = F.relu(h)
+        for conv in self.convs:
+            h = conv(h, edge_index)
+            h = F.Relu(h)
+        h = self.conv2(h, edge_index)
+        return F.log_softmax(h, dim=1)
+
+
+
+
