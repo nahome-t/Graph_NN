22cb398c862f0a689a2907e033c16d94eb8c22d8
nahometewolde <nahome.t@icloud.com>
2023-03-14 11:08:09 +0000

    Created class which produces message passing layer

--- models.py
+++ models.py
@@ -32,5 +32,41 @@ class GCN(nn.Module):
         return F.log_softmax(h, dim=1)
 
 
+class Norm_adj(MessagePassing):
+    # Effectively multiplying the feature vectors by the normalized
+    # augmented adjacency matrix or effectively a graph convolutional layer
+    # with W set to
+    # identity and no bias
+
+    # Equivalent to adding self loop to each node, summing over neighbours
+    # feature vectors such that two nodes of degree n1, n2 will also be
+    # normalised by (n1*n2)^(-0.5)
+
+    def __init__(self):
+        super().__init__(aggr='add') # Aggregates via adding
+
+    def forward(self, x, edge_index):
+        # Adds self loops
+        edge_index, _ = add_self_loops(edge_index, num_nodes=x.size(0))
+        row, col = edge_index
+
+        deg = degree(col, x.size(0), dtype=x.dtype)  # Calculates degree for
+        # each node
+        deg_inv_sqrt = deg.pow(-0.5)
+
+        # If node has 0 degree then normalisations set to 0 (shouldn't be
+        # case since self loops added anyway)? but implemented in graph
+        # convolution
+        deg_inv_sqrt[deg_inv_sqrt==float('inf')] = 0
+
+        norm = deg_inv_sqrt[row]*deg_inv_sqrt[col]
+        # Propagates values with normalisation
+        out = self.propagate(edge_index=edge_index, x=x, norm=norm)
+        return out
+
+    def message(self, x_j, norm):
+        return norm.view(-1, 1) * x_j # Normalizes value by node number
+
+
 
 
