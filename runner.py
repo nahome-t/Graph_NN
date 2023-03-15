6310fba06588dc6395b8e8cc6614fdca84beee49
nahometewolde <nahome.t@icloud.com>
2023-03-15 00:02:43 +0000

    Removed redundant import statements, placed emphasis that generate_mask is geared towards the Cora dataset, added some additional comments and made train2 try again if it can't find a model that is 100% accurate on the training dataset

--- runner.py
+++ runner.py
@@ -1,37 +1,48 @@
 from torch_geometric.datasets import Planetoid
 import torch
 
-import torch.nn as nn
-from torch_geometric.nn import GCNConv, MessagePassing
-from torch_geometric.utils import add_self_loops, degree
 import torch.nn.functional as F
-import time
+
 from models import GCN, GfNN
 
 
 learning_rate = 0.01 # Figure out good value?
-max_epochs = 50
+max_epochs = 50 # Model trained to 100% accuracy on training dataset,
+# maximum epochs represents
 hidden_layer_size = 16
-depth = 2
-
+GNN_type = 'GCN'
 dataset = Planetoid(root='/tmp/Cora', name='Cora')
 data = dataset[0] # Only one graph in this particular dataset so we just need
 # to load the first element
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+data = data.to(device)
 
-model = GCN(in_features=data.num_features,
-            out_features=dataset.num_classes,
-            depth=depth,
-            hidden_layer_size=hidden_layer_size)
-model, data = model.to(device), data.to(device)
+def generate_model(GNN_type, depth):
+    # Generates a model using the dataset in the function and the given
+    # parameters
+    if GNN_type == 'GCN':
+        model = GCN(in_features=data.num_features,
+                    out_features=dataset.num_classes,
+                    depth=depth,
+                    hidden_layer_size=hidden_layer_size)
+    elif GNN_type == 'GfNN':
+        model = GfNN(in_features=data.num_features,
+                    out_features=dataset.num_classes,
+                    k=depth,
+                    hidden_layer_size=hidden_layer_size)
+    return model.to(device)
 
-# class DataHandler()
+
 
 def generate_mask(data, mask):
     # Picks 140 examples from the testing data such that each class is
     # picked 20 times, note the testing data is only a subset of the entire
     # data and so the mask for the testing data needs to be given
+
+    # NOTE CLASS SIZE OF 20 SPECIFICALLY GEARED TOWARDS CORA DATASET
+
+    CLASS_SIZE = 20
 
     data = data.tolist()
     mask = mask.tolist()
@@ -41,7 +52,7 @@ def generate_mask(data, mask):
         freq[i] = 0
 
     for i, x in enumerate(data):
-        if mask[i] and freq[x]<20:
+        if mask[i] and freq[x]<CLASS_SIZE:
             freq[x]+=1
             test_mask[i] = True
 
@@ -70,6 +81,11 @@ def is_consistent(model, data, mask):
 
 
 def train2(model, data, mask):
+    # A slightly altered training method, model is trained to 100% accuracy
+    # on training data, this model is then applied to the mask given by one
+    # of the parameters (in this case the generated test mask, the output of
+    # this is then outputted
+
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     model.train()
     for i in range(max_epochs):
@@ -90,19 +106,18 @@ def train2(model, data, mask):
     model.eval()
 
     print(f'Model not found...')
+    # Tries again to find it if function not found within 50 epochs
+    return train2(model, data, mask)
 
-test_mask = generate_mask(data.y, data.test_mask)
 
-num_tests = 1
-test_size = 10  # How many tensors each test will store
+def run_simulation(filename):
+    return None
 
-test_size = 50
-for k in range(test_size):
-    model = GCN(in_features=data.num_features,
-                out_features=dataset.num_classes,
-                depth=depth,
-                hidden_layer_size=hidden_layer_size)
-    model = model.to(device)
-    print(train2(model, data, test_mask))
+    # test_mask = generate_mask(data.y, data.test_mask)
+    #
+    # test_size = 40  # How many times do we want to run each test
+    # for k in range(test_size):
+    #     model = model.to(device)
+    #     train2(model, data, test_mask)
 
 
